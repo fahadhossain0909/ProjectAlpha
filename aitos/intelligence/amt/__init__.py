6b903718e46edfa88b63d3fb0f7be122e91854fa
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-08-20 15:59:18 +0600

    feat(amt): export persistence and auction intent

--- aitos/intelligence/amt/__init__.py
+++ aitos/intelligence/amt/__init__.py
@@ -2,7 +2,8 @@
 from .engine import AMTContext, AMTEngine, AuctionState, DayType, ValueMigration
 from .profile_features import ProfileFeatures, compute_profile_features
 from .auction_intent import AuctionIntent, AuctionIntentResult, classify_auction_intent
+from .persistence import AMTClickHouseRepository
 from .session_store import SessionProfileStore, SessionSnapshot
 from .tpo_profile import TPOObservation, TPOProfile, build_tpo_profile
 from .volume_profile import VolumeProfile, build_volume_profile
-__all__ = ["AMTContext", "AMTEngine", "AuctionState", "DayType", "ValueMigration", "ProfileFeatures", "compute_profile_features", "AuctionIntent", "AuctionIntentResult", "classify_auction_intent", "SessionProfileStore", "SessionSnapshot", "TPOObservation", "TPOProfile", "build_tpo_profile", "VolumeProfile", "build_volume_profile"]
+__all__ = ["AMTContext", "AMTEngine", "AuctionState", "DayType", "ValueMigration", "ProfileFeatures", "compute_profile_features", "AuctionIntent", "AuctionIntentResult", "classify_auction_intent", "AMTClickHouseRepository", "SessionProfileStore", "SessionSnapshot", "TPOObservation", "TPOProfile", "build_tpo_profile", "VolumeProfile", "build_volume_profile"]
