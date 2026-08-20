dc4f482d2d3b7dbef65270b2c469a5679115dbe8
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-08-20 16:03:15 +0600

    feat(amt): export decision fusion API

--- aitos/intelligence/amt/__init__.py
+++ aitos/intelligence/amt/__init__.py
@@ -2,8 +2,9 @@
 from .engine import AMTContext, AMTEngine, AuctionState, DayType, ValueMigration
 from .profile_features import ProfileFeatures, compute_profile_features
 from .auction_intent import AuctionIntent, AuctionIntentResult, classify_auction_intent
+from .decision_fusion import AMTDecisionSignal, fuse_amt_context
 from .persistence import AMTClickHouseRepository
 from .session_store import SessionProfileStore, SessionSnapshot
 from .tpo_profile import TPOObservation, TPOProfile, build_tpo_profile
 from .volume_profile import VolumeProfile, build_volume_profile
-__all__ = ["AMTContext", "AMTEngine", "AuctionState", "DayType", "ValueMigration", "ProfileFeatures", "compute_profile_features", "AuctionIntent", "AuctionIntentResult", "classify_auction_intent", "AMTClickHouseRepository", "SessionProfileStore", "SessionSnapshot", "TPOObservation", "TPOProfile", "build_tpo_profile", "VolumeProfile", "build_volume_profile"]
+__all__ = ["AMTContext", "AMTEngine", "AuctionState", "DayType", "ValueMigration", "ProfileFeatures", "compute_profile_features", "AuctionIntent", "AuctionIntentResult", "classify_auction_intent", "AMTDecisionSignal", "fuse_amt_context", "AMTClickHouseRepository", "SessionProfileStore", "SessionSnapshot", "TPOObservation", "TPOProfile", "build_tpo_profile", "VolumeProfile", "build_volume_profile"]
