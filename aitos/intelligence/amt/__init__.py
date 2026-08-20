d0a7a8dfabcf2de0dffa2165ba1db270431bd967
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-08-20 07:48:36 +0600

    feat(amt): export auction intent classifier

--- aitos/intelligence/amt/__init__.py
+++ aitos/intelligence/amt/__init__.py
@@ -1,15 +1,8 @@
 """Production-grade Auction Market Theory primitives."""
-
 from .engine import AMTContext, AMTEngine, AuctionState, DayType, ValueMigration
 from .profile_features import ProfileFeatures, compute_profile_features
+from .auction_intent import AuctionIntent, AuctionIntentResult, classify_auction_intent
 from .session_store import SessionProfileStore, SessionSnapshot
 from .tpo_profile import TPOObservation, TPOProfile, build_tpo_profile
 from .volume_profile import VolumeProfile, build_volume_profile
-
-__all__ = [
-    "AMTContext", "AMTEngine", "AuctionState", "DayType", "ValueMigration",
-    "ProfileFeatures", "compute_profile_features",
-    "SessionProfileStore", "SessionSnapshot",
-    "TPOObservation", "TPOProfile", "build_tpo_profile",
-    "VolumeProfile", "build_volume_profile",
-]
+__all__ = ["AMTContext", "AMTEngine", "AuctionState", "DayType", "ValueMigration", "ProfileFeatures", "compute_profile_features", "AuctionIntent", "AuctionIntentResult", "classify_auction_intent", "SessionProfileStore", "SessionSnapshot", "TPOObservation", "TPOProfile", "build_tpo_profile", "VolumeProfile", "build_volume_profile"]
